b0f2e543c27e8986f1ce016d6680d8b5ef8958da
AndaZidele <87810405+AndaZidele@users.noreply.github.com>
2021-11-16 11:49:17 +0200

    restore

---
+++ WeekDayFinderGoodOne/Controllers/AppearenceViewController.swift
@@ -0,0 +1,48 @@
+//
+//  AppearenceViewController.swift
+//  WeekDayFinderGoodOne
+//
+//  Created by anda.zidele on 16/11/2021.
+//
+
+import UIKit
+
+class AppearenceViewController: UIViewController {
+
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func openSettingsButtonTapped(_ sender: Any) {
+    }
+    
+    func openSettings(){
+        guard let settingsURL = URL(string: UIApplication.openSettingsURLString) else {return}
+        
+        if UIApplication.shared.canOpenURL(settingsURL){
+            UIApplication.shared.open(settingsURL, options: [:]) { success in
+                print("open: ", success)
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
